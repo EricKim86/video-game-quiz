109f85e87d7c9844c7e98ccf43d02ddd326b7d29
EricKim86 <113047098+EricKim86@users.noreply.github.com>
2022-11-10 19:02:43 -0500

    created and updated style sheet

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var titleScreen = document.querySelector(".title");
 
 var mainScreen = document.querySelector(".main");
 
-var secondsLeft = 10;
+var secondsLeft = 60;
 
 //timer countdonw
 function setTime() {
@@ -25,19 +25,16 @@ function setTime() {
 
 //starting screen text
     score.textContent = "View High Scores";
-    titleScreen.textContent = "Video Game Quiz;"
+    titleScreen.textContent = "Video Game Quiz:"
     mainScreen.textContent = "Can you answer five video game related questions under 60 seconds?  Click Start Quiz to find out!";
 
 //Setting styles
-score.setAttribute("style", "color:blue; text-align:left; font-size:20px; font-family:impact;")
-timer.setAttribute("style", "color:blue; text-align:right; font-size:20px; font-family:impact;")
-titleScreen.setAttribute("style", "color:blue; text-align:center; font-size:35px; font-family:impact;")
-mainScreen.setAttribute("style", "color:black; text-align:center; font-size:20px; font-family:impact;")
     
 
 //gameover message
 function gameOverMessage() {
   timer.textContent = "";
+  titleScreen.textContent = "";
   var gameOver = document.createElement("img");
   gameOver.setAttribute("src", "assets/images/gameover-image.avif");
   mainScreen.appendChild(gameOver);
