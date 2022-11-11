4a9e5f1d87ba8c766797791e52b4c4b2890a8741
EricKim86 <113047098+EricKim86@users.noreply.github.com>
2022-11-11 10:36:51 -0500

    question now populates properly, changed format for answers again.

--- script.js
+++ script.js
@@ -1,32 +1,78 @@
+
+//main screen
 var startButton = document.getElementById("start-game")
 
 var questionBox = document.getElementById("question-box")
 
-var currentQuestion
+var welecomeMessage = document.getElementById("welcome-message")
 
-var score = document.querySelector(".scoreboard");
+//score and timer
 
-var timer = document.querySelector(".time");
+var score = document.querySelector(".scoreboard")
 
-var titleScreen = document.querySelector(".title");
-
-var mainScreen = document.querySelector(".main");
-
-var question = document.querySelector("#question");
-
-var options = Array.from(document.querySelector(".choice-text"));
+var timer = document.querySelector(".time")
 
 var secondsLeft = 60;
 
-startButton.addEventListener("click", startGame)
+var titleScreen = document.querySelector(".title")
 
-//start game
-function startGame() {
+var mainScreen = document.querySelector(".main")
+
+//questions and answers
+
+var choices = document.querySelectorAll(".choice-text")
+
+var questionSelect = document.getElementById("question")
+
+startButton.addEventListener("click", startGame)
 
 //starting screen text
 score.textContent = "View High Scores";
 
-// timer countdown
+//question list 0-4
+var questions = {
+    title: "Who is the main character in the Legend of Zelda series?",
+    options: ["Zelda", "Link", "Ganon", "navi",], 
+    answer: 2,
+  };
+  // {
+  //   question: "What year was the original Super Mario Bros. released for the Nintendo Entertainment System?",
+  //   option1: "1985",
+  //   option2: "1988",
+  //   option3: "1990",
+  //   option4: "1995",
+  //   answer: 1,
+  // },
+  // {
+  //   question: "How many Pokemon were there in Generation I?",
+  //   option1: "101",
+  //   option2: "121",
+  //   option3: "151",
+  //   option4: "201",
+  //   answer: 3,
+  // },
+  // {
+  //   question: "Who is the main character in the Metroid series?",
+  //   option1: "Samus Aran",
+  //   option2: "Star Fox",
+  //   option3: "Captain Falcon",
+  //   option4: "Marth",
+  //   answer: 1,
+  // },
+  // {
+  //   question: "As of Nov. 2022, what is the highest selling Nintendo game (units sold)?",
+  //   option1: "Animal Crossing: New Horizons",
+  //   option2: "Super Smash Bros. Ultimate",
+  //   option3: "Super Mario Odyssey",
+  //   option4: "Mario Kart 8 Deluxe",
+  //   answer: 4,
+  // },
+
+//start game
+function startGame() {
+  questionSelect.textContent = questions.title;
+
+// timer countdown - tick tock!
   var timerInterval = setInterval(function() {
     secondsLeft--;
     timer.textContent = "Time: " + secondsLeft;
@@ -38,60 +84,18 @@ score.textContent = "View High Scores";
     }
   }, 1000);
 
+// hide start button and welcome message
+
   startButton.classList.add("hide")
+  welecomeMessage.classList.add("hide")
   questionBox.classList.remove("hide")
-  currentQuestion = 0
-  nextQuestion()
 }
 
 // gameover message
-function gameOverMessage() {
-  timer.textContent = "";
-  titleScreen.textContent = "";
-  var gameOver = document.createElement("img");
-  gameOver.setAttribute("src", "assets/images/gameover-image.avif");
-  mainScreen.appendChild(gameOver);
-}
-
-var questions = [
-  {
-    question: "Who is the main character in the Legend of Zelda series?",
-    option1: "Zelda",
-    option2: "Link",
-    option3: "Ganon",
-    option4: "Navi",
-    answer: 2,
-  },
-  {
-    question: "What year was the original Super Mario Bros. released for the Nintendo Entertainment System?",
-    option1: "1985",
-    option2: "1988",
-    option3: "1990",
-    option4: "1995",
-    answer: 1,
-  },
-  {
-    question: "How many Pokemon were there in Generation I?",
-    option1: "101",
-    option2: "121",
-    option3: "151",
-    option4: "201",
-    answer: 3,
-  },
-  {
-    question: "Who is the main character in the Metroid series?",
-    option1: "Samus Aran",
-    option2: "Star Fox",
-    option3: "Captain Falcon",
-    option4: "Marth",
-    answer: 1,
-  },
-  {
-    question: "As of Nov. 2022, what is the highest selling Nintendo game (units sold)?",
-    option1: "Animal Crossing: New Horizons",
-    option2: "Super Smash Bros. Ultimate",
-    option3: "Super Mario Odyssey",
-    option4: "Mario Kart 8 Deluxe",
-    answer: 4,
-  },
-]
+// function gameOverMessage() {
+//   timer.textContent = "";
+//   titleScreen.textContent = "";
+//   var gameOver = document.createElement("img");
+//   gameOver.setAttribute("src", "assets/images/gameover-image.avif");
+//   mainScreen.appendChild(gameOver);
+// }
