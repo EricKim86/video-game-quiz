0141c90150ae43c4eeb2d3db8382df8d65a6c6e4
EricKim86 <113047098+EricKim86@users.noreply.github.com>
2022-11-11 15:00:15 -0500

    cleaned up styles, added next question code

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var questionBox = document.getElementById("question-box")
 
 var welecomeMessage = document.getElementById("welcome-message")
 
+
 //score and timer
 
 var score = document.querySelector(".scoreboard")
@@ -24,52 +25,68 @@ var choices = document.querySelectorAll(".choice-text")
 
 var questionSelect = document.getElementById("question")
 
+var correctAnswer = document.getElementById("correct")
+
+var incorrectAnswer = document.getElementById("incorrect")
+
+var nextButton = document.getElementById("next-button")
+
 startButton.addEventListener("click", startGame)
 
 //starting screen text
 score.textContent = "View High Scores";
 
 //question list 0-4
-var questions = {
+  var question1 = {
     title: "Who is the main character in the Legend of Zelda series?",
-    options: ["Zelda", "Link", "Ganon", "navi",], 
+    options: ["Zelda", "Link", "Ganon", "Navi"], 
     answer: 1,
-  };
-  // {
-  //   title: "What year was the original Super Mario Bros. released for the Nintendo Entertainment System?",
-  //   option: ["1985", "1988", "1990", "1995",]
-  //   answer: 0,
-  // },
-  // {
-  //   title: "How many Pokemon were there in Generation I?",
-  //   option: ["101", "121", "151", "201",]
-  //   answer: 2,
-  // },
-  // {
-  //   title: "Who is the main character in the Metroid series?",
-  //   option: "Samus Aran", "Star Fox", "Captain Falcon", "Marth"]
-  //   answer: 0,
-  // },
-  // {
-  //   title: "As of Nov. 2022, what is the highest selling Nintendo game (units sold)?",
-  //   option: "Animal Crossing: New Horizons", "Super Smash Bros. Ultimate", "Super Mario Odyssey", "Mario Kart 8 Deluxe",]
-  //   answer: 3,
-  // },
+  }
+  var question2 = {
+    title: "What year was the original Super Mario Bros. released for the Nintendo Entertainment System?",
+    options: ["1985", "1988", "1990", "1995"],
+    answer: 0,
+  }
+  var question3 = {
+    title: "How many Pokemon were there in Generation I?",
+    options: ["101", "121", "151", "201"],
+    answer: 2,
+  }
+  var question4= {
+    title: "Who is the main character in the Metroid series?",
+    options: ["Samus Aran", "Star Fox", "Captain Falcon", "Marth"],
+    answer: 0,
+  }
+  var question5 = {
+    title: "As of Nov. 2022, what is the highest selling Nintendo game (units sold)?",
+    options: ["Animal Crossing: New Horizons", "Super Smash Bros. Ultimate", "Super Mario Odyssey", "Mario Kart 8 Deluxe"],
+    answer: 3,
+  }
+
 
 //start game
 function startGame() {
-  questionSelect.textContent = questions.title;
+  questionSelect.textContent = question1.title;
   choices.forEach(function(element, index) {
-      element.textContent = questions.options[index];
+      element.textContent = question1.options[index];
 
       element.addEventListener("click", function() {
-        if(questions.answer == index) {
-          console.log("Correct!");
+        if(question1.answer == index) {
+          correctAnswer.textContent = "Correct!";
+          correctAnswer.classList.remove("hide")
+          incorrectAnswer.classList.add("hide")
+          nextButton.classList.remove("hide")
         } else {
           console.log("Incorrect!");
+          incorrectAnswer.textContent = "Incorrect! -10 seconds";
+          incorrectAnswer.classList.remove("hide")
+          correctAnswer.classList.add("hide")
+          nextButton.classList.remove("hide")
         }
         });
       });
+      nextButton.addEventListener("click", nextQuestion)
+
 // timer countdown - tick tock!
   var timerInterval = setInterval(function() {
     secondsLeft--;
@@ -86,6 +103,35 @@ function startGame() {
   startButton.classList.add("hide")
   welecomeMessage.classList.add("hide")
   questionBox.classList.remove("hide")
+}
+
+function nextQuestion() {
+//clear the page for the next question
+  correctAnswer.classList.add("hide")
+  incorrectAnswer.classList.add("hide")
+  nextButton.classList.add("hide")
+
+//next question
+  questionSelect.textContent = question2.title;
+  choices.forEach(function(element, index) {
+      element.textContent = question2.options[index];
+
+      element.addEventListener("click", function() {
+        if(question2.answer == index) {
+          correctAnswer.textContent = "Correct!";
+          correctAnswer.classList.remove("hide")
+          incorrectAnswer.classList.add("hide")
+          nextButton.classList.remove("hide")
+        } else {
+          console.log("Incorrect!");
+          incorrectAnswer.textContent = "Incorrect! -10 seconds";
+          incorrectAnswer.classList.remove("hide")
+          correctAnswer.classList.add("hide")
+          nextButton.classList.remove("hide")
+        }
+        });
+      });
+      nextButton.addEventListener("click", nextQuestion)
 }
 
 // gameover message
