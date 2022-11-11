719244c98c28130a2644348e88e0d6754d1c6097
EricKim86 <113047098+EricKim86@users.noreply.github.com>
2022-11-10 20:30:52 -0500

    added question framework

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var startButton = document.getElementById("start-game")
+
+var questionBox = document.getElementById("question-box")
+
+var currentQuestion
+
 var score = document.querySelector(".scoreboard");
 
 var timer = document.querySelector(".time");
@@ -8,37 +14,49 @@ var mainScreen = document.querySelector(".main");
 
 var secondsLeft = 60;
 
-//timer countdonw
-function setTime() {
+startButton.addEventListener("click", startGame)
 
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timer.textContent = "Time: " + secondsLeft;
-
-    if(secondsLeft === 0) {
-      clearInterval(timerInterval);
-      mainScreen.textContent = "";
-      gameOverMessage();
-    }
-  }, 1000);
+//start game
+function startGame() {
+  startButton.classList.add("hide")
+  questionBox.classList.remove("hide")
+  currentQuestion = 0
+  nextQuestion()
 }
-
 //starting screen text
     score.textContent = "View High Scores";
-    titleScreen.textContent = "Video Game Quiz:"
-    mainScreen.textContent = "Can you answer five video game related questions under 60 seconds?  Click Start Quiz to find out!";
 
-//Setting styles
-    
+// timer countdown
 
-//gameover message
-function gameOverMessage() {
-  timer.textContent = "";
-  titleScreen.textContent = "";
-  var gameOver = document.createElement("img");
-  gameOver.setAttribute("src", "assets/images/gameover-image.avif");
-  mainScreen.appendChild(gameOver);
+//   var timerInterval = setInterval(function() {
+//     secondsLeft--;
+//     timer.textContent = "Time: " + secondsLeft;
 
-}
+//     if(secondsLeft === 0) {
+//       clearInterval(timerInterval);
+//       mainScreen.textContent = "";
+//       gameOverMessage();
+//     }
+//   }, 1000);
+// }
 
-setTime();
+// gameover message
+// function gameOverMessage() {
+//   timer.textContent = "";
+//   titleScreen.textContent = "";
+//   var gameOver = document.createElement("img");
+//   gameOver.setAttribute("src", "assets/images/gameover-image.avif");
+//   mainScreen.appendChild(gameOver);
+// }
+
+var questions = [
+  {
+    question: "Who is the main character in the Legend of Zelda series?",
+    opetions: [
+        {text: "Link", correct: true},
+        {text: "Zelda", correct: false},
+        {text: "Ganon", correct: false},
+        {text: "Navi", correct: false},
+    ]
+  }
+]
